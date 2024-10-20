851320c80dc16960be30abf034db6fe5c51dec99
Hind Ainous <ainoushind12@gmail.com>
2024-10-20 20:14:46 +0200

    Calculating densities and forces between each particle
    Signed-off-by: Hind Ainous <ainoushind12@gmail.com>

--- Assets/Scenes/Circle.cs
+++ Assets/Scenes/Circle.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
+using Unity.Jobs;
+using UnityEngine.Jobs;
+using System.Threading.Tasks;
 
 public class Circle : MonoBehaviour
 {
     //Circle properties
     [Range(0, 10)]
-    public float radius = 1f;
+    public float radius = 0.1f;
+    public float smoothingRadius = 0.5f;
     public int segments = 100;
     public float collisionDampening = 0.82f;
 
+    public float density = 0f;
+
+    public float targetDensity;
+    public float pressureMultiplier;
+
+    public float deltaTime = 1f;
+
+    public float mass = 1.0f;
+
     //Circles properties
+    public float[] densities;
     private Vector3[] velocity;
     private Vector3[] positions;
     public int particleCount = 1;
@@ -32,6 +47,8 @@ public class Circle : MonoBehaviour
         positions = new Vector3[particleCount];
         velocity = new Vector3[particleCount];
 
+        densities = new float[particleCount];
+
         float minX = boundsSize.x / 2 * -1 + radius;
         float maxX = boundsSize.x / 2 - radius;
 
@@ -47,19 +64,111 @@ public class Circle : MonoBehaviour
         rp = new RenderParams(material);
     }
 
+    Vector2 CalculatePressureForce(int particleIndex)
+    {
+        Vector2 pressureForce = Vector2.zero;
+
+        for (int i = 0; i < particleCount; i++)
+        {
+            if (i == particleIndex) continue;
+            Vector3 offset = positions[i] - positions[particleIndex];
+            float dst = offset.magnitude;
+            Vector2 dir = dst == 0 ? new Vector2(1,0) : offset / dst;
+            float slope = SmoothingKernelDerivative(dst, smoothingRadius);
+            float density = densities[i];
+            pressureForce += -ConvertDensityToPressure(density) * dir * slope * mass / density;
+        }
+
+        return pressureForce;
+    }
+
+    void UpdateDensities()
+    {
+        Parallel.For(0, particleCount, i =>
+        {
+            densities[i] = CalculateDensity(positions[i]);
+        });
+    }
+
+    float ConvertDensityToPressure(float density)
+    {
+        float densityError = density - targetDensity;
+        float pressure = density * pressureMultiplier;
+        return pressure;
+    }
+
+    float SmoothingKernel(float radius, float density)
+    {
+        float volume = Mathf.PI * Mathf.Pow(radius, 8) / 4;
+        float value = Mathf.Max(0, radius*radius - density*density);
+        return value * value * value / volume;
+    }
+
+    static float SmoothingKernelDerivative(float dst, float radius)
+    {
+        if (dst >= radius) return 0;
+        float f = radius * radius - dst * dst;
+        float scale = -24 / (Mathf.PI * Mathf.Pow(radius, 8));
+        return scale * dst * f * f;
+    }
+
+    float CalculateDensity(Vector3 particlePosition)
+    {
+        float density = 0;
+
+        foreach(Vector3 position in positions)
+        {
+            float dst = (position - particlePosition).magnitude;
+            float influence = SmoothingKernel(smoothingRadius, dst);
+            density += mass * influence;
+        }
+
+        return density;
+    }
+
     void Update()
     {
-        for (int i = 0; i < positions.Length; i++)
+        SimulationStep(deltaTime);
+        /*for (int i = 0; i < positions.Length; i++)
         {
             ResolveCollision(i);
 
             velocity[i] += Vector3.down * gravity * Time.deltaTime;
             positions[i] += velocity[i] * Time.deltaTime;
             Graphics.RenderMesh(rp, mesh, 0, Matrix4x4.Translate(positions[i]));
+        }*/
+
+        for(int i = 0; i < particleCount; i++)
+        {
+            Graphics.RenderMesh(rp, mesh, 0, Matrix4x4.Translate(positions[i]));
         }
 
-        mesh = CreateCircleMesh(radius, segments);
+        //density = CalculateDensity();
 
+        //mesh = CreateCircleMesh(radius, segments);
+
+    }
+
+    void SimulationStep(float deltaTime)
+    {
+        Parallel.For(0, particleCount, i =>
+        {
+            velocity[i] += Vector3.down * gravity * deltaTime;
+            densities[i] = CalculateDensity(positions[i]);
+        });
+        
+        Parallel.For(0, particleCount, i =>
+        {
+            Vector3 pressureForce = -CalculatePressureForce(i);
+            Vector3 pressureAcceleration = pressureForce / densities[i];
+            velocity[i] += pressureAcceleration * deltaTime;
+        });
+
+        Parallel.For(0, particleCount, i =>
+        {
+            positions[i] += velocity[i] * deltaTime;
+            ResolveCollision(i);
+        });
     }
 
     void ResolveCollision(int arrayPosition)
