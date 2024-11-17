b6d8a51ea0e857a9dc31bec640e2a2b1bc5d3d13
Hind Ainous <ainoushind12@gmail.com>
2024-11-17 21:46:18 +0100

    Improvement on the basics
    Signed-off-by: Hind Ainous <ainoushind12@gmail.com>

--- Assets/Scenes/Circle.cs
+++ Assets/Scenes/Circle.cs
@@ -6,17 +6,38 @@ using UnityEngine;
 using UnityEngine.UIElements;
 using Unity.Jobs;
 using UnityEngine.Jobs;
+using System;
 using System.Threading.Tasks;
+using System.Threading;
+using System.Drawing;
+using UnityEngine.SocialPlatforms;
 
 public class Circle : MonoBehaviour
 {
+    //Time settings
+    public float timeScale = 1;
+    public int iterationsPerFrame;
+    float deltaTime = 0.0001f;
+
+
     //Gizmos 
     public bool drawGizmosBoundry = true;
     public bool drawGizmosGrid = true;
-
+    Tuple<int, int>[] cellOffsets = new Tuple<int, int>[]
+    {
+        Tuple.Create(-1, 1),
+        Tuple.Create(0, 1),
+        Tuple.Create(1, 1),
+        Tuple.Create(-1, 0),
+        Tuple.Create(0, 0),
+        Tuple.Create(1, 0),
+        Tuple.Create(-1, -1),
+        Tuple.Create(0, -1),
+        Tuple.Create(1, -1),
+    };
     //Circle properties
     [Range(0, 10)]
-    public float radius = 0.1f;
+    public float circleRadius = 0.1f;
     public float smoothingRadius = 0.5f;
     public int segments = 100;
     public float collisionDampening = 0.82f;
@@ -24,16 +45,14 @@ public class Circle : MonoBehaviour
     public float targetDensity;
     public float pressureMultiplier;
 
-    [Range(0.0001f, 0.1f)]
-    public float deltaTime = 0.0001f;
-
     public float mass = 1.0f;
-    public float RandomX = 1.0f;
-    public float RandomY = 1.0f;
 
     //Circles properties
-    public float[] densities;
+    private float[] densities;
+    public int[] startIndices;
+    private Entry[] spatialLookup;
     private Vector3[] velocity;
+    private Vector3[] points;
     private Vector3[] positions;
     private Vector3[] predictedPositions;
     public int particleCount = 1;
@@ -53,24 +72,31 @@ public class Circle : MonoBehaviour
         positions = new Vector3[particleCount];
         velocity = new Vector3[particleCount];
         predictedPositions = new Vector3[particleCount];
+        points = new Vector3[particleCount];
+        spatialLookup = new Entry[particleCount];
+        startIndices = new int[particleCount];
 
         densities = new float[particleCount];
 
-        RandomX = Random.Range(-0.5f, 0.5f);
-        RandomY = Random.Range(-0.5f, 0.5f);
+        float minX = boundsSize.x / 2 * -1 + circleRadius;
+        float maxX = boundsSize.x / 2 - circleRadius;
 
-        float minX = boundsSize.x / 2 * -1 + radius;
-        float maxX = boundsSize.x / 2 - radius;
+        float minY = boundsSize.y / 2 * -1 + circleRadius;
+        float maxY = boundsSize.y / 2 - circleRadius;
 
-        float minY = boundsSize.y / 2 * -1 + radius;
-        float maxY = boundsSize.y / 2 - radius;
+        int particlesPerRow = (int)Mathf.Sqrt(particleCount);
+        int particlesPerCol = (particleCount -1) / particlesPerRow + 1;
+        float spacing = circleRadius * 2 + 0.01f;
 
         for (int i = 0; i < particleCount; i++)
         {
-            positions[i] = new Vector3(Random.Range(minX, maxX), Random.Range(minY, maxY), 0);    
+            float x = (i % particlesPerRow - particlesPerRow / 2f + 0.5f) * spacing;
+            float y = (i / particlesPerRow - particlesPerCol / 2f + 0.5f) * spacing;
+            //positions[i] = new Vector3(UnityEngine.Random.Range(minX, maxX), UnityEngine.Random.Range(minY, maxY), 0);    
+            positions[i] = new Vector3(x, y, 0);
         }
 
-        mesh = CreateCircleMesh(radius, segments);
+        mesh = CreateCircleMesh(circleRadius, segments);
         rp = new RenderParams(material);
     }
 
@@ -84,7 +110,7 @@ public class Circle : MonoBehaviour
             Vector3 offset = positions[i] - positions[particleIndex];
             float dst = offset.magnitude;
             if (dst > smoothingRadius) continue;
-            Vector2 dir = dst == 0 ? new Vector2(RandomX, RandomY) : offset / dst;
+            Vector2 dir = dst == 0 ? new Vector2(0, 1) : offset / dst;
             float slope = SmoothingKernelDerivative(dst, smoothingRadius);
             float density = densities[i];
             float sharedPressure = CalculateSharedPressure(density, densities[particleIndex]);
@@ -105,7 +131,7 @@ public class Circle : MonoBehaviour
     {
         Parallel.For(0, particleCount, i =>
         {
-            densities[i] = CalculateDensity(positions[i]);
+            densities[i] = CalculateDensity(predictedPositions[i]);
         });
     }
 
@@ -148,35 +174,50 @@ public class Circle : MonoBehaviour
 
     void Update()
     {
-        SimulationStep(deltaTime);
-
         for (int i = 0; i < particleCount; i++)
         {
+            Graphics.RenderMesh(rp, mesh, 0, Matrix4x4.Translate(positions[i]));
+        }
 
-            RenderParams rp2 = new RenderParams(material);
-            Graphics.RenderMesh(rp2, mesh, 0, Matrix4x4.Translate(positions[i]));
+        if (Time.frameCount > 10)
+        {
+            RunSimulationFrame(Time.deltaTime);
         }
     }
 
-    void SimulationStep(float deltaTime)
+    void RunSimulationFrame(float frameTime)
+    {
+        float timeStep = frameTime / iterationsPerFrame * timeScale;
+
+        deltaTime = timeStep;
+
+        for (int i = 0; i < iterationsPerFrame; i++)
+        {
+            SimulationStep();
+        }
+    }
+
+    void SimulationStep()
     {
         Parallel.For(0, particleCount, i =>
         {
             velocity[i] += Vector3.down * gravity * deltaTime;
-            predictedPositions[i] = positions[i] + velocity[i] * deltaTime;
+            predictedPositions[i] = positions[i] + velocity[i] * 1 / 120f;
         });
 
-        Parallel.For(0, particleCount, i =>
-        {
-            densities[i] = CalculateDensity(predictedPositions[i]);
-        });
-        
+        //UpdateSpatialLookUp(predictedPositions, smoothingRadius);
+
+
+        UpdateDensities();
+
+
         Parallel.For(0, particleCount, i =>
         {
             Vector3 pressureForce = CalculatePressureForce(i);
             Vector3 pressureAcceleration = pressureForce / densities[i];
             velocity[i] += pressureAcceleration * deltaTime;
         });
+
 
         Parallel.For(0, particleCount, i =>
         {
@@ -185,9 +226,64 @@ public class Circle : MonoBehaviour
         });
     }
 
+    public void UpdateSpatialLookUp(Vector3[] points, float radius)
+    {
+        this.points = points;
+
+        Parallel.For(0, points.Length, i =>
+        {
+            (int cellX, int cellY) = PositionToCellCoord(points[i], radius);
+            uint cellKey = CellKeyFromHash(HashCell(cellX, cellY));
+            spatialLookup[i] = new Entry(i, cellKey);
+            startIndices[i] = int.MaxValue;
+        });
+
+        Array.Sort(spatialLookup);
+
+        Parallel.For(0, points.Length, i =>
+        {
+            uint key = spatialLookup[i].cellKey;
+            uint keyPrev = i == 0 ? uint.MaxValue : spatialLookup[i - 1].cellKey;
+            if (key != keyPrev)
+            {
+                startIndices[key] = i;
+            }
+        });
+    }
+
+    public (int x, int y) PositionToCellCoord(Vector3 point, float radius)
+    {
+        int cellX = (int)(point.x / radius);
+        int cellY = (int)(point.y / radius);
+        return (cellX, cellY);
+    }
+
+    /*public void ForeachPointWithinRadius(Vector3 samplePoint)
+    {
+        (int centreX, int centreY) = PositionToCellCoord(samplePoint, smoothingRadius);
+        float sqrRadius = smoothingRadius * smoothingRadius;
+
+        foreach((int offsetX, int offsetY) in cellOffsets)
+        {
+            uint key = CellKeyFromHash(HashCell(centreX + offsetX, centreY + offsetY));
+            int cellStartIndex = startIndices[key];
+
+            for (int i = cellStartIndex; i < spatialLookup.Length; i++)
+            {
+                if (spatialLookup[i].cellKey != key) break;
+
+                int particleIndex = spatialLookup[i].num;
+                float sqrDst = (points[particleIndex] - samplePoint).sqrMagnitude;
+                if (sqrDst <= sqrRadius)
+                {
+                }
+            }
+        }
+    }*/
+
     void ResolveCollision(int arrayPosition)
     {
-        Vector2 halfBoundSize = boundsSize / 2 - Vector2.one * radius;
+        Vector2 halfBoundSize = boundsSize / 2 - Vector2.one * circleRadius;
 
         if (Mathf.Abs(positions[arrayPosition].x) > halfBoundSize.x)
         {
@@ -208,14 +304,14 @@ public class Circle : MonoBehaviour
         if (drawGizmosBoundry == true)
         {
             // Draw a yellow sphere at the transform's position
-            Gizmos.color = Color.yellow;
+            Gizmos.color = UnityEngine.Color.yellow;
             Gizmos.DrawWireCube(new Vector3(0, 0, 0), size);
         }
 
         if(drawGizmosGrid == true)
         {
             Vector3 cellSize = new Vector3(smoothingRadius * 2, smoothingRadius * 2);
-            Gizmos.color = Color.green;
+            Gizmos.color = UnityEngine.Color.green;
 
             for (int i = 0; i < boundsSize.x; i++)
             {
@@ -234,16 +330,16 @@ public class Circle : MonoBehaviour
         return (cellX, cellY);
     }
 
-    public uint HashKey(int x, int y)
+    public uint HashCell(int x, int y)
     {
         uint a = (uint)x * 103387;
         uint b = (uint)y * 96763;
         return a + b;
     }
 
-    public uint CellKey(uint hash)
+    public uint CellKeyFromHash(uint hash)
     {
-        return hash % (uint)positions.Length;
+        return hash % (uint)spatialLookup.Length;
     }
 
     Mesh CreateCircleMesh(float radius, int segments)
@@ -276,4 +372,50 @@ public class Circle : MonoBehaviour
 
         return mesh;
     }
+}
+
+public struct Entry : IComparable<Entry>
+{
+    public Entry(int x, uint y)
+    {
+        num = x;
+        cellKey = y;
+    }
+
+    public int num { get; }
+    public uint cellKey { get; }
+
+    public int CompareTo(Entry other)
+    {
+        // Example: Compare based on the X coordinate first, then Y coordinate
+        if (num != other.num)
+        {
+            return num.CompareTo(other.num);
+        }
+        return cellKey.CompareTo(other.cellKey);
+    }
+
+    // Override Equals for value-based equality
+    public override bool Equals(object obj)
+    {
+        if (obj is Entry other)
+        {
+            return num == other.num && cellKey == other.cellKey;
+        }
+        return false;
+    }
+
+    // Override GetHashCode
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(num, cellKey);
+    }
+
+    // Overload comparison operators
+    public static bool operator ==(Entry p1, Entry p2) => p1.Equals(p2);
+    public static bool operator !=(Entry p1, Entry p2) => !p1.Equals(p2);
+    public static bool operator <(Entry p1, Entry p2) => p1.CompareTo(p2) < 0;
+    public static bool operator >(Entry p1, Entry p2) => p1.CompareTo(p2) > 0;
+    public static bool operator <=(Entry p1, Entry p2) => p1.CompareTo(p2) <= 0;
+    public static bool operator >=(Entry p1, Entry p2) => p1.CompareTo(p2) >= 0;
 }
